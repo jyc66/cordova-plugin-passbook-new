0d3fd141bd52ef82a8893df459071bfec17b07ea
Joe Reda <joe.reda@gmail.com>
2022-01-18 13:28:02 -0500

    promises everywhere

--- www/passbook.js
+++ www/passbook.js
@@ -1,6 +1,6 @@
 var passbook = exports;
 
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
 /**
  * Pass
@@ -11,14 +11,13 @@ var exec = require('cordova/exec');
  * @constructor
  */
 var Pass = function (passTypeIdentifier, serialNumber, passURL) {
-    this.passTypeIdentifier = passTypeIdentifier || null;
-    this.serialNumber = serialNumber || null;
-    this.passURL = passURL || null;
+  this.passTypeIdentifier = passTypeIdentifier || null;
+  this.serialNumber = serialNumber || null;
+  this.passURL = passURL || null;
 };
 
-
 Pass.prototype.open = function () {
-    passbook.openPass(this.passURL, null, null);
+  passbook.openPass(this.passURL, null, null);
 };
 
 passbook.Pass = Pass;
@@ -27,49 +26,96 @@ passbook.Pass = Pass;
  *
  * @param resultCallback {Function} is called with result
  */
-passbook.available = function (resultCallback) {
-    exec(resultCallback, null, "Passbook", "available", []);
+passbook.available = function () {
+  return new Promise((resolve, reject) => {
+    exec(
+      function (result) {
+        resolve(result);
+      },
+      function (err) {
+        reject(err);
+      },
+      "Passbook",
+      "available",
+      []
+    );
+  });
 };
 
 /**
  * @param {Object}  { passIdentifier:String, passSerial:String }
  * @param resultCallback {Function} is called with result
  */
- passbook.exists = function (callData, resultCallback) {
-    exec(resultCallback, null, "Passbook", "passExists", [callData]);
+passbook.exists = function (callData) {
+  return new Promise((resolve, reject) => {
+    exec(
+      function (result) {
+        resolve(result);
+      },
+      function (err) {
+        reject(err);
+      },
+      "Passbook",
+      "passExists",
+      [callData]
+    );
+  });
 };
 
 /**
  *
  * @param {Object}  url:String | { url:String, headers?:Object }
- * @param {Function} passCallback
- * @param {Function} errorCallback
  */
-passbook.downloadPass = function (callData, passCallback, errorCallback) {
-    return new Promise(
-        (resolve, reject) => {
-            exec(function (result) {
-                if (typeof(passCallback) === 'function') {
-                    var pass = result.pass;
-                    resolve({pass: new Pass(pass.passTypeIdentifier, pass.serialNumber, pass.passURL), isAdded: result.added});
-                }
-            }, function(err){reject(err)}, "Passbook", "downloadPass", [callData]);
-    });
+passbook.downloadPass = function (callData) {
+  return new Promise((resolve, reject) => {
+    exec(
+      function (result) {
+        var pass = result.pass;
+        resolve({
+          pass: new Pass(
+            pass.passTypeIdentifier,
+            pass.serialNumber,
+            pass.passURL
+          ),
+          isAdded: result.added,
+        });
+      },
+      function (err) {
+        reject(err);
+      },
+      "Passbook",
+      "downloadPass",
+      [callData]
+    );
+  });
 };
 
 /**
  *
  * @param file Local File URL, e.g. file:///path/pass.pkpass
- * @param {Function} passCallback
- * @param {Function} errorCallback
  */
-passbook.addPass = function (file, passCallback, errorCallback) {
-    exec(function (result) {
-        if (typeof(passCallback) === 'function') {
-            var pass = result.pass;
-            passCallback(new Pass(pass.passTypeIdentifier, pass.serialNumber, pass.passURL), result.added);
-        }
-    }, errorCallback, "Passbook", "addPass", [file]);
+passbook.addPass = function (file) {
+  return new Promise((resolve, reject) => {
+    exec(
+      function (result) {
+        var pass = result.pass;
+        resolve({
+          pass: new Pass(
+            pass.passTypeIdentifier,
+            pass.serialNumber,
+            pass.passURL
+          ),
+          isAdded: result.added,
+        });
+      },
+      function (err) {
+        reject(err);
+      },
+      "Passbook",
+      "addPass",
+      [file]
+    );
+  });
 };
 
 /**
@@ -78,6 +124,18 @@ passbook.addPass = function (file, passCallback, errorCallback) {
  * @param {Function} successCallback
  * @param {Function} errorCallback
  */
-passbook.openPass = function (passOrUrl, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Passbook", "openPass", [passOrUrl]);
+passbook.openPass = function (passOrUrl) {
+  return new Promise((resolve, reject) => {
+    exec(
+      function (result) {
+        resolve(result);
+      },
+      function (err) {
+        reject(err);
+      },
+      "Passbook",
+      "openPass",
+      [passOrUrl]
+    );
+  });
 };
